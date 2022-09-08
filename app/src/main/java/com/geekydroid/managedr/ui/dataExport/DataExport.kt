625e847d446f793b398219a030920cde7acfdaa1
nagarajasunder <gowthamvj15@gmail.com>
2022-09-08 09:05:53 +0530

    Added total amounts to the excel sheet

--- app/src/main/java/com/geekydroid/managedr/ui/dataExport/DataExport.kt
+++ app/src/main/java/com/geekydroid/managedr/ui/dataExport/DataExport.kt
@@ -1,19 +1,25 @@
 package com.geekydroid.managedr.ui.dataExport
 
+import android.util.Log
 import com.geekydroid.managedr.ui.settings.model.ExportDoctorData
 import com.geekydroid.managedr.utils.TextUtils
 import org.apache.poi.hssf.usermodel.HSSFSheet
 import org.apache.poi.hssf.usermodel.HSSFWorkbook
 import org.apache.poi.ss.usermodel.Row
 
+private const val TAG = "DataExport"
 object DataExport {
 
+    var ROW_NUMBER = 0
+    val EXTRA_LINE_SPACE = 1
+    val MOVE_ROW_TO_NEXT_LINE = 1
 
     suspend fun createWorkBook(data: List<ExportDoctorData>, cityNames: List<String>) : HSSFWorkbook {
         val workBook = HSSFWorkbook()
         val sheets = createSheetsForEachCity(cityNames, workBook)
         sheets.forEach { sheet ->
             writePerDoctorData(sheet, data.filter { it.cityName == sheet.sheetName })
+            ROW_NUMBER = 0
         }
         return workBook
     }
@@ -23,58 +29,60 @@ object DataExport {
         data: List<ExportDoctorData>,
     ) {
         val perDoctorData = data.groupBy { it.doctorName }
-        var rowNumber = 0
         perDoctorData.forEach {
-            writeDoctorData(rowNumber, it.key, it.value, sheet)
-            rowNumber += it.value.size + 5
+            writeDoctorData(it.key, it.value, sheet)
         }
     }
 
     private suspend fun writeDoctorData(
-        startRowNumber: Int,
         doctorName: String,
         data: List<ExportDoctorData>,
         sheet: HSSFSheet,
     ) {
-        var rowNumber = startRowNumber
+        Log.d(TAG, "writeDoctorData: For city ${sheet.sheetName}")
+        Log.d(TAG, "writeDoctorData: doctor data $data")
         val totalServiceAmount = data.filter { it.transactionType == "SERVICE" }.sumOf { serviceList -> serviceList.transactionAmount }
         val totalReturnAmount = data.filter { it.transactionType == "COLLECTION" }.sumOf { collectionList -> collectionList.transactionAmount }
-        writeDoctorName(rowNumber++, sheet, doctorName)
-        writeSheetHeaders(rowNumber++, sheet, listOf("S.No",
+        Log.d(TAG, "writeDoctorData: $totalServiceAmount service amount")
+        Log.d(TAG, "writeDoctorData: $totalReturnAmount return amount")
+        writeDoctorName(sheet, doctorName)
+        writeSheetHeaders(sheet, listOf("S.No",
             "Date of Transaction",
             "Transaction Type",
             "City",
             "Category",
             "Amount"))
-        writeDoctorTransactions(rowNumber,sheet,data)
-        rowNumber = data.size+1
-        writeDoctorTransactionTotals(totalServiceAmount,totalReturnAmount,rowNumber,sheet)
+        writeDoctorTransactions(sheet,data)
+//        ROW_NUMBER += data.size+2
+        writeDoctorTransactionTotals(totalServiceAmount,totalReturnAmount,sheet)
     }
 
     private fun writeDoctorTransactionTotals(
         totalServiceAmount: Double,
         totalReturnAmount: Double,
-        startRowNumber: Int,
         sheet: HSSFSheet,
     ) {
-        var rowNumber = startRowNumber
-        var row = sheet.createRow(rowNumber++)
+        var row = sheet.createRow(ROW_NUMBER)
+        ROW_NUMBER+= MOVE_ROW_TO_NEXT_LINE
         createCell(row,4,"Total Service Amount")
         createCell(row,5,TextUtils.getCurrencyFormat(totalServiceAmount))
-        row = sheet.createRow(rowNumber)
+        row = sheet.createRow(ROW_NUMBER)
+        ROW_NUMBER+= MOVE_ROW_TO_NEXT_LINE
         createCell(row,4,"Total Return Amount")
         createCell(row,5,TextUtils.getCurrencyFormat(totalReturnAmount))
+        row = sheet.createRow(ROW_NUMBER)
+        ROW_NUMBER+= MOVE_ROW_TO_NEXT_LINE
+        createCell(row,4,"Total Transaction Amount")
+        createCell(row,5,TextUtils.getCurrencyFormat((totalServiceAmount+totalReturnAmount)))
     }
 
-    private suspend fun writeDoctorTransactions(
-        rowNumber: Int,
+    private fun writeDoctorTransactions(
         sheet: HSSFSheet,
         data: List<ExportDoctorData>,
     ) {
-        var rowIndex = rowNumber
         var serialNo = 1
         data.forEach { transaction ->
-            val row = sheet.createRow(rowIndex)
+            val row = sheet.createRow(ROW_NUMBER)
             var columnNumber = 0
             createCell(row,columnNumber++,(serialNo++).toString())
             createCell(row,columnNumber++,transaction.transactionDateFormatted)
@@ -82,18 +90,20 @@ object DataExport {
             createCell(row,columnNumber++,transaction.cityName)
             createCell(row,columnNumber++,transaction.divisionName)
             createCell(row,columnNumber,transaction.transactionAmountFormatted)
-            rowIndex++
+            ROW_NUMBER+= MOVE_ROW_TO_NEXT_LINE
         }
+        ROW_NUMBER+= EXTRA_LINE_SPACE
     }
 
-    private fun writeSheetHeaders(rowNumber: Int, sheet: HSSFSheet, headerNames: List<String>) {
-        val row = sheet.createRow(rowNumber)
+    private fun writeSheetHeaders(sheet: HSSFSheet, headerNames: List<String>) {
+        val row = sheet.createRow(ROW_NUMBER)
         val columnWidth = 15*500
         headerNames.forEachIndexed{ index,value ->
             sheet.setColumnWidth(index,columnWidth)
             val cell = row.createCell(index)
             cell.setCellValue(value)
         }
+        ROW_NUMBER+= MOVE_ROW_TO_NEXT_LINE
     }
 
     private fun createCell(row:Row, columnNumber: Int, value: String) {
@@ -101,11 +111,11 @@ object DataExport {
         cell.setCellValue(value)
     }
 
-    private suspend fun writeDoctorName(startRowNumber: Int, sheet: HSSFSheet, doctorName: String) {
-        val row = sheet.createRow(startRowNumber)
+    private suspend fun writeDoctorName(sheet: HSSFSheet, doctorName: String) {
+        val row = sheet.createRow(ROW_NUMBER)
         val cell = row.createCell(0)
         cell.setCellValue(doctorName)
-
+        ROW_NUMBER+= MOVE_ROW_TO_NEXT_LINE
     }
 
     private suspend fun createSheetsForEachCity(cityNames: List<String>, workbook: HSSFWorkbook): List<HSSFSheet> {
