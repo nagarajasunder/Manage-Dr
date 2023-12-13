730cad6a653ce3f23c422a948b72b84480c8d6e7
nagarajasunder <gowthamvj15@gmail.com>
2023-12-13 09:12:47 +0530

    ui-tests espresso core gradle setup with sample UI tests

--- app/src/androidTest/java/com/geekydroid/managedr/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/geekydroid/managedr/ExampleInstrumentedTest.kt
@@ -1,12 +1,21 @@
 package com.geekydroid.managedr
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.geekydroid.managedr.ui.LandingActivity
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -14,11 +23,16 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
+@LargeTest
 class ExampleInstrumentedTest {
+
+
+    @get:Rule
+    val activityScenarioRule = ActivityScenarioRule(LandingActivity::class.java)
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.geekydroid.managedr", appContext.packageName)
+        onView(withId(R.id.tv_empty_list_text)).check(matches(withText("No Doctors Found!")))
     }
 }
